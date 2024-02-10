5917df1a7fbe4a0e71941acf08716b85a889dd8e
ItsMeSamey <sameychain5041@gmail.com>
2024-02-10 14:55:33 +0530

    edi

--- public/js/work.js
+++ public/js/work.js
@@ -1,5 +1,57 @@
 let ID;
 let FS = {};
+let KEY;
+let FILES = [];
+let PASSWORD;
+
+function jsonLoad(){
+  try {KEY = }catch{}
+  if (KEY){
+    let response;
+    gapi.client.drive.files.get({
+      fileId: FS['id'],
+      alt: "media"
+    }).then(function(res) { 
+      FILES = JSON.parse{decrypt(res.body, CryptoJS.SHA256(PASSWORD).toString(), false)};
+    });
+  }
+}
+
+function jsonWrite(){
+  if (KEY){
+    response = await gapi.client.files.delete({
+      'fileId': KEY
+    });
+  }
+
+    file = JSON.toString(),
+    var metadata = {
+      'mimeType': 'text/plain',
+      'name': "L.txt",
+      'uploadType': 'media',
+      'parents': [ID],
+      'useContentAsIndexableText': false,
+      'body': file,
+    };
+
+    var form = new FormData();
+    form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+    form.append('file', file);
+
+    var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+      var xhr = new XMLHttpRequest();
+    xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      KEY = xhr.response.id;
+      document.getElementById('content').style.display = 'block';
+    };
+    xhr.send(form);
+    FILES.push(FP);
+  }
+}
+
 
 function handleAuthClick() {
   tokenClient.callback = async (resp) => {
@@ -17,7 +69,6 @@ function handleAuthClick() {
   }
 }
 
-
 function handleSignoutClick() {
   const token = gapi.client.getToken();
   if (token !== null) {
@@ -25,7 +76,6 @@ function handleSignoutClick() {
     gapi.client.setToken('');
   }
 }
-
 
 async function makeFolder() {
   let response;
@@ -36,10 +86,11 @@ async function makeFolder() {
       'pageSize': 1,
     });
   } catch (err) { // ERR!!!
-      return;
   }
 
-  var files = response.result.files;
+
+
+  files = response.result.files;
 
 
   if (!files || files.length == 0) {
@@ -52,13 +103,27 @@ async function makeFolder() {
   }
   ID = files[0].id;
 
+  try {
+    response = await gapi.client.drive.files.list({
+      'q': 'mimeType=\'application/vnd.google-apps.folder\'',
+      'q': 'name=\'2330759051.04ed9bf428c4ac12196c75a9cea2a629dfb0e28fdyxorpipa\'',
+      'pageSize': 1,
+    });
+  } catch (err) { // ERR!!!
+        response = await gapi.client.drive.files.create({
+      'mimeType': 'plain/text',
+      'name': 'apiproxydf82e0bfd926a2aec9a57c69121ca4c824fb9de40.1509570332',
+      'parents': [ID],
+    });
+    KEY = response.result.files;
+  }
 }
 
 async function uploadFile() {
   const fileInput = document.getElementById('fileInput');
   let file = fileInput.files[0];
   if (file) {
-
+    FS['name'] = file.name;
     const hashedPwd = CryptoJS.SHA256(CryptoJS.lib.WordArray.random(256).toString()).toString();
     FS['hash'] = hashedPwd;
     file = await file.text();
@@ -88,17 +153,35 @@ async function uploadFile() {
       document.getElementById('content').style.display = 'block';
     };
     xhr.send(form);
+    FILES.push(FP);
+    jsonWrite();
+    FP = {};
   } else {
     console.error("No file selected.");
   }
 }
 
-async function downloadFile(){
+async function downloadFile(FILE){
   gapi.client.drive.files.get({
-    fileId: FS['id'],
+    fileId: FILE['id'],
     alt: "media"
   }).then(function(res) { 
-    let file = decrypt(res.body, FS['hash'], false);
+    let file = decrypt(res.body, FILE['hash'], false);
+    const blob = new Blob([file], { type: 'text/plain' });
+
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+
+    link.download = 'variable.txt';
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
   });
 }
 
